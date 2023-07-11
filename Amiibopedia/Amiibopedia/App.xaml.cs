be5b3557bceb08fdbcc8bafd021f0954dd2d2b52
AlbertoJTD <albertojtd@outlook.com>
2023-07-10 18:55:01 -0600

    Add test data to MainPage.xaml

--- Amiibopedia/Amiibopedia/App.xaml.cs
+++ Amiibopedia/Amiibopedia/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Amiibopedia
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+			MainPage = new NavigationPage(new MainPage());
 		}
 
 		protected override void OnStart()
